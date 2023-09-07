8336d65a9f1400a91eaa674511aa53338a6f5de1
GitHub Action <action@github.com>
2023-09-07 09:34:55 +0000

    Update swagger

--- rest/xuanwu/swagger/client/client/operations/hello_responses.go
+++ rest/xuanwu/swagger/client/client/operations/hello_responses.go
@@ -36,7 +36,7 @@ func (o *HelloReader) ReadResponse(response runtime.ClientResponse, consumer run
 		}
 		return nil, result
 	default:
-		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
+		return nil, runtime.NewAPIError("[GET /hello] hello", response, response.Code())
 	}
 }
 
